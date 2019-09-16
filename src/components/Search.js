1b22fa613dab2efaaf91745272a03c12900973e9
Raphaelli Bellini <raphaelli.bellini@novatendencia.localdomain>
2019-09-16 09:44:41 -0300

    Create function to add repository

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,41 @@ import { Grid, Segment, Input, Button } from 'semantic-ui-react';
 import api from '../services/api'
 
 class Search extends Component {
+    state = {
+        query: '',
+        repoInfo: {},
+        repositories: []
+    }
+
+    updateQuery = (query) => {
+        this.setState ({ query });
+    }
+
+    addRepository = async() => {
+        try{
+            console.log(this.state.query);
+            const resp = await api.get(`/repos/${this.state.query}`);
+            console.log(resp);
+            const { owner: { avatar_url, login }, name, stargazers_count, language, forks } = resp.data;
+
+            this.state.repositories.push({
+                avatar_url,
+                name,
+                login,
+                stargazers_count,
+                language,
+                forks
+            })
+            console.log(this.state.repositories);
+
+            
+        } catch {
+            const error = document.querySelector('.error');
+            error.style.display = 'block';
+        }
+        
+    }
+
     render() {
         return (
             <Grid>
@@ -14,10 +49,10 @@ class Search extends Component {
 
                 <Grid.Row columns={3} verticalAlign='middle'>
                     <Grid.Column width={2}>
-                        <img alt='Logo do GitHub' src='https://avatars3.githubusercontent.com/u/69631?v=4' className='logo-git'></img>
+                        <img alt='Logo do Github' src="https://img.icons8.com/ios-glyphs/60/000000/github.png"></img>
                     </Grid.Column>
                     <Grid.Column width={13} className='search-header'>
-                        Repositorios
+                        Repositórios
                     </Grid.Column>
                     <Grid.Column width={1} textAlign='right' className='search-header'>
                         4
@@ -30,10 +65,12 @@ class Search extends Component {
                             focus  
                             className='search-bar'
                             type='text'
+                            value={this.state.query}
+                            onChange={(evt) => this.updateQuery(evt.target.value)}
                         />
                     </Grid.Column>
                     <Grid.Column width={4}>
-                        <Button color='purple' className='search-button'>ADD</Button>
+                        <Button color='purple' className='search-button' onClick={this.addRepository}>ADD</Button>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
